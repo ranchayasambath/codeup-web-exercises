b365ec16be0f2eee91ad4ee3130a953b3ab35c95
ranchayasambath <ranchayasambath@gmail.com>
2021-11-27 20:16:42 -0600

    finished simple exercise

--- js/functionDrills.js
+++ js/functionDrills.js
@@ -304,5 +304,35 @@ function rollDice(sides){
 }
 console.log(rollDice(6));
 
+//SIMPLE FUNCTION DRILLS
 
-
+//1.Make a function called returnTwo() that returns the number 2 when called
+//Test this function with console.log(returnTwo()):
+function returnTwo(){
+    return(2);
+}
+console.log(returnTwo());
+//2.Make a function called sayHowdy() which console.logs the string “Howdy!”
+// Test this function by directly calling sayHowdy()
+function sayHowdy(){
+    return("Howdy!");
+}
+console.log(sayHowdy());
+//3.Make a function called returnName() that returns the string of your name
+// Test this function with console.log(returnName())
+function returnName(){
+    return("Jack");
+}
+console.log(returnName());
+//4.Make a function called addThree() which takes in a number input and returns the number plus 3.
+// Test this function with console.log(addThree(5))
+function addThree(i){
+    return(i+3);
+}
+console.log(addThree(2));
+//5.Make a function called sayString() which returns the string input passed in.
+// Test this function with console.log(sayString('codeup'))
+function sayString(str){
+    return("Hello, "+str);
+}
+console.log(sayString("codeup"));
