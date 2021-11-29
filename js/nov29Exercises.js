27ca02d5ea7bbfa6d1213c21d3159e7024aff270
ranchayasambath <ranchayasambath@gmail.com>
2021-11-29 14:45:11 -0600

    class exercises

--- js/nov29Exercises.js
+++ js/nov29Exercises.js
@@ -16,11 +16,28 @@ function grades(num) {
         return("F");
     }
 }
-console.log(grades(0));
+console.log(grades(100));
 // it runs perfectly without && statements. (Theory of why is >= (or equal to)  acts as a ceiling that prevents it
 //from being read as the code passes down.)
 
 
-let num= 90;
-var ask= prompt("Whats did you get on the test?");
-ask === num ? alert("Congratulations you got an A!"): alert("Sorry you didnt get an A.")
+//let num= 90;
+//var ask= prompt("Whats did you get on the test?");
+//ask === num ? alert("Congratulations you got an A!"): alert("Sorry you didnt get an A.")
+var str = ("10");
+switch(str){
+    case "90":
+        console.log("You got an A");
+        break;
+    case "80":
+        console.log("You got an B");
+        break;
+    case "70":
+        console.log("You got an C");
+        break;
+    case "60":
+        console.log("You got an D");
+        break;
+    default:
+        console.log("You got an F");
+}
