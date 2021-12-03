9de439209abe4dfa66d4fe01fe385554a2b0bd65
ranchayasambath <ranchayasambath@gmail.com>
2021-12-03 10:01:27 -0600

    completed while exercises

--- js/while.js
+++ js/while.js
@@ -3,3 +3,15 @@ let i= 1;
 while (i<= 65536 -1)
     console.log(i*=2);
 //--------------------------------------------
+var allCones = Math.floor(Math.random() * 50 +50)
+do{
+    var conesToPurchase = Math.floor(Math.random()*5)+1;
+    if (conesToPurchase <= allCones){
+        console.log(conesToPurchase + " cones sold...")
+        allCones -= conesToPurchase
+    } else {
+        console.log("Cannot sell you" + conesToPurchase + "cones I have" + allCones + " ....")
+    }
+console.log(allCones)
+}while(allCones > 0)
+console.log("Yay! I sold all the cones!")
