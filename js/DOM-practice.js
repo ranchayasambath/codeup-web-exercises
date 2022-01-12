a9978c4496efa55793769c8f2b8bcc4d9c5a1bd7
ranchayasambath <ranchayasambath@gmail.com>
2022-01-12 11:20:39 -0600

    updated exercise

---
+++ js/DOM-practice.js
@@ -0,0 +1,30 @@
+// var promptAnswer = prompt ( "what's your name?");
+// var retrievedSpan = document.getElementById("name");
+
+// console.log("prompAnswer", promptAnswer);
+// console.log("retrievedSpan", retrievedSpan);
+
+
+// retrievedSpan.innerHTML = promptAnswer;
+// retrievedSpan.textContent = promptAnswer;
+
+
+//  var promptAnswer = prompt ( "hows the weather?");
+// var newParagraph = document.getElementById("sentence");
+// newParagraph.innerHTML = " The weather is " + promptAnswer;
+//
+
+// 1.declare a variable
+// 2.document.getElementsByClassName
+// 3.specify the class we're looking for  ---> odd
+// 4.console.log the variable to make sure I'm grabbing the info I neeed
+
+// RETRIEVING FORM INPUT ELEMENTS
+//1. create a variable to store input value
+//2.retrieve the input value
+//2a. object notation to retrieve that value
+//
+// var userInput = document.forms["login"].username;
+// console.log(userInput.value);
+// var inputVal = userInput.value;
+// console.log(inputVal);
