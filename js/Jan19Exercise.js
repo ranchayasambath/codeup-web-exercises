180eb51d8d92bd7405316426ee3f0e042ab80a8f
ranchayasambath <ranchayasambath@gmail.com>
2022-01-19 12:26:00 -0600

    in-class exercise

---
+++ js/Jan19Exercise.js
@@ -0,0 +1,11 @@
+document.getElementById("heading");
+
+//jQuery
+let myEl= $("#p-one");
+console.log(myEl);
+
+let heading = $("#heading").text();
+console.log(heading);
+console.log($("#heading").html());
+$("#p-one").addClass("border");
+
