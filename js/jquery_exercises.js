96efe41335fb1e585da859535deef699614d2c00
ranchayasambath <ranchayasambath@gmail.com>
2022-01-20 10:51:43 -0600

    completed Jquery selecter exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,21 @@
 "user strict";
 
 $(document).ready(function(){
-    alert( 'The DOM has finished loading!' );
+    // alert( 'The DOM has finished loading!' );
 });
+// var hello = $("#hello").html();
+// alert(hello);
+// var world = $("#world").html();
+// alert(world);
+
+// $(".codeup").css({"border":"solid 1px", "color": "red"});
+
+$("li").css("font-size","20px");
+
+// $("h1").css({"border":"solid 1px", "color":"blue", "background-color":"orange" });
+// $("p").css({"border":"solid 1px", "color":"blue", "background-color":"orange" });
+// $("li").css({"border":"solid 1px", "color":"blue", "background-color":"orange" });
+
+$("h1,p,li").css({"border":"solid 1px", "color":"blue", "background-color":"orange" });
+var heading = $("h1").html();
+alert (heading);
