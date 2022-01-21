91bae04b8f9950b8d39d36b06ebcc640fa6d6db4
ranchayasambath <ranchayasambath@gmail.com>
2022-01-20 19:28:57 -0600

    embeded web contra game into html from js file

--- js/konami.js
+++ js/konami.js
@@ -5,11 +5,17 @@ $(document).keyup(function(event){
 });
     var input = "";
     var key = '38384040373937396665';
-    $(document).on('keydown', function (event) {
-        input = input + (event.keyCode);
-        if (input === key) {
+    $(document).on('keydown', function(event) {
+        input+= (event.keyCode);
+        if (input === key){
             alert("Cheat Activated");
             audio.play();
+            $("#game").html('<iframe id="game" src="https://www.retrogames.cc/embed/16841-contra-usa.html" width="600" height="450" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe>')
+            // document.getElementById("game").innerHTML='<iframe id="game" ' +
+            //     'src="https://www.retrogames.cc/embed/16841-contra-usa.html" width="600" ' +
+            //     'height="450" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" ' +
+            //     'mozallowfullscreen="true" scrolling="no"></iframe>'
+
         }
         if (!key.indexOf(input)) return;
         return input = ("" + event.keyCode);
