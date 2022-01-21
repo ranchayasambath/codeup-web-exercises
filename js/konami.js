007ab5a86150dce02aeec6f474700ba81a10a212
ranchayasambath <ranchayasambath@gmail.com>
2022-01-21 13:06:12 -0600

    Updated Exercise

--- js/konami.js
+++ js/konami.js
@@ -4,12 +4,13 @@ $(document).keyup(function(event){
     console.log(event.keyCode);
 });
     var input = "";
-    var key = '38384040373937396665';
-    $(document).on('keydown', function(event) {
+    var key = '3838404037393739666513';
+    $(document).on('keyup', function(event) {
         input+= (event.keyCode);
+        console.log(input)
         if (input === key){
             alert("Cheat Activated");
-            audio.play();
+            // audio.play();
             $("#game").html('<iframe id="game" src="https://www.retrogames.cc/embed/16841-contra-usa.html" width="600" height="450" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe>')
 
             // document.getElementById("game").innerHTML='<iframe id="game" ' +
@@ -18,7 +19,9 @@ $(document).keyup(function(event){
             //     'mozallowfullscreen="true" scrolling="no"></iframe>'
 
         }
+        // if the first input is not the same as the first key clear the input;
         if (!key.indexOf(input)) return;
+        // then  return any key pressed back to an empty string
         return input = ("" + event.keyCode);
     });
 
