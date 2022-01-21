96d38174b80ae70c056dfd659160dbcee2eb0987
ranchayasambath <ranchayasambath@gmail.com>
2022-01-20 21:24:21 -0600

    Updated Exercises

--- js/Jan20Exercise.js
+++ js/Jan20Exercise.js
@@ -1,5 +1,6 @@
 var str="bJIENsdfRAIRVIrsRFAijgRar";
-// var i=0;
+var i=0;
+// -------------while loop-------------
 // while ( i < str.length){
 //     let char = str.charAt(i);
 //     if (char === char.toLowerCase()){
@@ -9,12 +10,26 @@ var str="bJIENsdfRAIRVIrsRFAijgRar";
 //     }i++;
 // };
 
-for (let i=0 ; i< str.length; i++){
-    // var char = str.charAt(i)
-    if(str.charAt(i) === str.charAt(i).toLowerCase()){
-        console.log(str.charAt(i) + " is lower case.");
+// ----------------- do while loop------------------
+do {
+    if(str.charAt(i)=== str.charAt(i).toLowerCase()){
+        console.log(str.charAt(i) +" is lower case.")
     }else{
         console.log(str.charAt(i) + " is Upper case.")
     }
-};
+    i++;
 
+}  while(i < str.length);
+
+
+
+// --------------------- for loop-------------------
+// for (let i=0 ; i< str.length; i++){
+//     // var char = str.charAt(i)
+//     if(str.charAt(i) === str.charAt(i).toLowerCase()){
+//         console.log(str.charAt(i) + " is lower case.");
+//     }else{
+//         console.log(str.charAt(i) + " is Upper case.")
+//     }
+// };
+//
--- js/konami.js
+++ js/konami.js
@@ -11,6 +11,7 @@ $(document).keyup(function(event){
             alert("Cheat Activated");
             audio.play();
             $("#game").html('<iframe id="game" src="https://www.retrogames.cc/embed/16841-contra-usa.html" width="600" height="450" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe>')
+
             // document.getElementById("game").innerHTML='<iframe id="game" ' +
             //     'src="https://www.retrogames.cc/embed/16841-contra-usa.html" width="600" ' +
             //     'height="450" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" ' +
