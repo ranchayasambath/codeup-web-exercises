4fc2225d0fb36be6c218472887cf8068608f1af4
ranchayasambath <ranchayasambath@gmail.com>
2022-01-20 18:43:22 -0600

    completed keyboard event exercise

--- js/konami.js
+++ js/konami.js
@@ -3,3 +3,18 @@
 $(document).keyup(function(event){
     console.log(event.keyCode);
 });
+    var input = "";
+    var key = '38384040373937396665';
+    $(document).on('keydown', function (event) {
+        input = input + (event.keyCode);
+        if (input === key) {
+            alert("Cheat Activated");
+            audio.play();
+        }
+        if (!key.indexOf(input)) return;
+        return input = ("" + event.keyCode);
+    });
+
+
+
+
