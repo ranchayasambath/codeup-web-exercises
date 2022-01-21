e47582de62fc76df3381161b4fc34e0211ee8396
ranchayasambath <ranchayasambath@gmail.com>
2022-01-21 15:58:06 -0600

    Konami Code Revision

--- js/konami.js
+++ js/konami.js
@@ -6,23 +6,36 @@ $(document).keyup(function(event){
     var input = "";
     var key = '3838404037393739666513';
     $(document).on('keydown', function(event) {
-        input+= (event.keyCode);
-        console.log(input)
+        input = input + (event.keyCode);
+        console.log(input);
         if (input === key){
-            alert("Cheat Activated");
-            // audio.play();
+            alert("You unlocked 30 Lives");
             $("#game").html('<iframe id="game" src="https://www.retrogames.cc/embed/16841-contra-usa.html" width="600" height="450" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe>')
-
-            // document.getElementById("game").innerHTML='<iframe id="game" ' +
-            //     'src="https://www.retrogames.cc/embed/16841-contra-usa.html" width="600" ' +
-            //     'height="450" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" ' +
-            //     'mozallowfullscreen="true" scrolling="no"></iframe>'
-
+            $("body").css("background-color","black");
+            $(document).off();
+        }if(input.length > key.length){
+            alert("Wrong password, Try again");
+            location.reload();
+        }if(input.key === "r"){
+            location.reload();
         }
-        // if the sequence of  input is not the same as the first key clear the input;
-        if (!key.indexOf(input)) return;
-        // then  return any key pressed back to an empty string
-        return input = ("" + event.keyCode);
+
+
+
+
+
+
+        // input+= (event.keyCode);
+        // console.log(input)
+        // if (input === key){
+        //     alert("Cheat Activated");
+        //     // audio.play();
+        //     $("#game").html('<iframe id="game" src="https://www.retrogames.cc/embed/16841-contra-usa.html" width="600" height="450" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe>')
+        // }
+        // // if the sequence of  input is not the same as the first key clear the input;
+        // if (!key.indexOf(input)) return;
+        // // then  return any key pressed back to an empty string
+        // return input = ("" + event.keyCode);
     });
 
 
